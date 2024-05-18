cf5796bf25cee99afed9a6978e38b694b7c4afc2
Ayoub_prod <64814867+ayoubFromTheHood@users.noreply.github.com>
2024-05-18 02:39:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteCompression from 'vite-plugin-compression';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  return {
-    plugins: [
-      react(),
-      viteCompression()
-    ],
-    assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
-    base: './'
-  }
-});
+export default defineConfig({
+  plugins: [react()],
+  base: "./"
+})
+
